1db6acfe237e3afc800d699376bfd92c451e0644
YuanLang <75685359+nev3da@users.noreply.github.com>
2024-10-02 08:20:37 +0000

    init

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+let myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+
+let myImage = document.querySelector("img");
+
+myImage.onclick = function () {
+    let mySrc = myImage.getAttribute("src");
+    if (mySrc === "imgs/OIP.jpg") {
+        myImage.setAttribute("src", "imgs/test.png");
+    } else {
+        myImage.setAttribute("src", "imgs/OIP.jpg");
+    }
+};
+
+let myButton = document.querySelector("button");
+function setUserName() {
+    let myName = prompt("请输入你的名字。");
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem("name", myName);
+        myHeading.textContent = "Mozilla 酷毙了，" + myName;
+    }
+
+}
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem("name");
+    myHeading.textContent = "Hello, " + storedName;
+}
+myButton.onclick = () => {
+    setUserName();
+};
