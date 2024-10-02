9d21fa40a33c2d642cd98e46535ac9ec8b19cd47
YuanLang <75685359+nev3da@users.noreply.github.com>
2024-10-02 16:31:42 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -19,7 +19,7 @@ function setUserName() {
         setUserName();
     } else {
         localStorage.setItem("name", myName);
-        myHeading.textContent = "Mozilla 酷毙了，" + myName;
+        myHeading.textContent = "Hello, " + myName;
     }
 
 }
